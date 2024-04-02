97ed36dee26e952315f36875f1dec476567cbc52
Wboy0010 <143533874+AllMightyTekno@users.noreply.github.com>
2024-04-02 19:49:52 +0100

    Update on Resize plugin Roblox

--- UiResizeHandler.lua
+++ UiResizeHandler.lua
@@ -1,25 +1,16 @@
 ---//Services
 
 local StudioService = game:GetService("StudioService")
-
 local Players = game:GetService("Players")
-
 local Selection = game:GetService("Selection")
-
 local UserInputService = game:GetService("UserInputService")
-
+local RunService = game:GetService("RunService")
 
 
 --//Variables
-
 local loggedInUsrId = StudioService:GetUserId() --UsrId who is logged in Studio 
-
 local loggedInUsrName = Players:GetNameFromUserIdAsync(loggedInUsrId) --Name of the the usr logged in studio
-
 local SelectedObjects = Selection:Get() --Get's an array of selected instances in roblox studio
-
-
-
 local toolbar = plugin:CreateToolbar("ResizeMe")
 
 
@@ -33,11 +24,8 @@ PluginButton.ClickableWhenViewportHidden = true
 
 
 ---////CHECKS IF ALL CONNECTIONS ARE WORKING
-
 print(loggedInUsrId)
-
 print(loggedInUsrName)
-
 ---////CHECKS
 
 
@@ -51,16 +39,11 @@ print(loggedInUsrName)
 
 
 for _, object in ipairs(SelectedObjects) do --Loops through all the Objects gotten under the selectObjects Variable 
-
- 
-
-    if object:IsA("Frame") then --Checks if the selected object is a frame
-
-       local obj = object
+local Object = object
 
 
-
-        local initialSize = obj.Size
+    if Object:IsA("Frame") then --Checks if the selected object is a frame
+        local initialSize = Object.Size
 
 local function updateFrameSize(input)
 
@@ -68,14 +51,13 @@ local function updateFrameSize(input)
 
     local mouseDelta = input.Delta
 
-
-
     -- Update the frame size based on mouse movements
-
-    obj.Size = UDim2.new(initialSize.X.Scale, initialSize.X.Offset + mouseDelta.X, 
-
-                           initialSize.Y.Scale, initialSize.Y.Offset + mouseDelta.Y)
-
+RunService.RenderStepped:Connect(function()
+   Object.Size = UDim2.new(initialSize.X.Scale, initialSize.X.Offset + mouseDelta.X,  initialSize.Y.Scale, initialSize.Y.Offset + mouseDelta.Y)
+    print(Object.Size)            
+                           
+end)
+  
 end
 
 
@@ -85,13 +67,10 @@ end
 -- Connect to the InputChanged event to capture mouse movements
 
 UserInputService.InputChanged:Connect(function(input)
-
     -- Check if the input is mouse movement
 
     if input.UserInputType == Enum.UserInputType.MouseMovement then
-
         -- Call the updateFrameSize function
-
         updateFrameSize(input)
 
     end
